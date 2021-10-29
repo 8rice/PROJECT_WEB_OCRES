a5d53811fce000ddb6fea3b24de8f22b276e1e8a
8rice <bricesabuco@gmail.com>
2021-10-29 11:21:37 +0200

    Nav

---
+++ frontend/src/Containers/Nav.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class Nav extends React.Component {
+
+    state = {
+        data: []
+    }
+
+    componentDidMount() {
+        fetch("https://v3.football.api-sports.io/players?league=61&search=payet", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "f362f7602bfd94a69f2ccee18732fbd2",
+            }
+        }).then((response) => {
+            return response.json()
+        })
+            .then((result) => {
+                this.setState(
+                    { data: result.response[0].player.name }
+                )
+                console.log(result.response[0].player.name)
+            })
+    }
+
+
+    render() {
+        return (
+            <div>
+                Hello
+                <p> {this.state.data}</p>
+            </div >
+        );
+    }
+}
