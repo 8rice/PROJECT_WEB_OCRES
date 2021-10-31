d00adb2d6ca72ae9959dca3f39acdff784247465
8rice <bricesabuco@gmail.com>
2021-10-31 14:03:14 +0100

    TopBar name surname

--- frontend/src/Containers/TopBar.js
+++ frontend/src/Containers/TopBar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-export default class TopBar extends React.Component {
+export default function TopBar() {
 
-    render() {
-        return (
-            <div className="TopBar">
-                <h1 className="Logo">OffSide LOGO</h1>
-                <div className="UserInfos">
-                    <p>Name</p>
-                    <img alt="userPicture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png" />
-                </div>
-            </div>
-        );
+    const user = {
+        name: "Jacques",
+        surname: "Rossard"
     }
+
+    return (
+        <div className="TopBar">
+            <h1 className="Logo">OffSide LOGO</h1>
+            <div className="UserInfos">
+                <p>{user.name} {user.surname}</p>
+                <img alt="userPicture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png" />
+            </div>
+        </div>
+    );
+
 }
