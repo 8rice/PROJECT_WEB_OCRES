3cf264b74fc1112abcb2cb38b930d7db396f0cdc
Yannis Goore <yannis.goore@edu.ece.fr>
2021-10-31 14:11:39 +0100

    Logo of the website

--- frontend/src/Containers/TopBar.js
+++ frontend/src/Containers/TopBar.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import '../Assets/logo.png'
 
 export default class TopBar extends React.Component {
 
     render() {
         return (
             <div className="TopBar">
-                <h1 className="Logo">OffSide LOGO</h1>
+                 <div className="Logo">
+                <img alt="logo" src="{logo}"/> 
+                </div>
+                
                 <div className="UserInfos">
                     <p>Name</p>
                     <img alt="userPicture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png" />
