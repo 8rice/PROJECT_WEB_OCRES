13d3adaff7bc2bf28aaf7bf4010df260835b3c1b
Yannis Goore <yannis.goore@edu.ece.fr>
2021-11-14 20:02:21 +0100

    tableau finition

--- frontend/src/Containers/TopScorerWidget.js
+++ frontend/src/Containers/TopScorerWidget.js
@@ -4,41 +4,43 @@
 import React, { useState } from 'react'
 
 function TopScorerWidget() {
-    const [data, setData] = useState(null);
-
-    function componentDidMount() {
-        fetch("https://v3.football.api-sports.io/players/topscorers?league=61&season=2020", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "f362f7602bfd94a69f2ccee18732fbd2",
-            }
-        }).then((response) => {
-            return response.json()
-        })
-            .then((result) => {
-                console.log(result.response[0].player.name)
-                setData(result.response[0].player.name)
-            })
-    }
+    /* const [data, setData] = useState(null);
+ 
+     function componentDidMount() {
+ 
+         fetch("https://v3.football.api-sports.io/players/topscorers?league=61&season=2020", {
+             "method": "GET",
+             "headers": {
+                 "x-rapidapi-key": "f362f7602bfd94a69f2ccee18732fbd2",
+             }
+         }).then((response) => {
+             return response.json()
+         })
+             .then((result) => {
+                 console.log(result.response[0].player.name)
+                 setData(result.response[0].player.name)
+             })
+     }
+  
     componentDidMount();
+       */
     return (
         <div className='TopScorer'>
             <table>
                 <thead>
-                    <tr><th className='Title' colspan="3"> <span>Best</span> SCORER</th></tr>
-
-                    <tr><th className='ColPlayer' scope="col"> <span>Player</span></th>
-                        <th className='Colgoals' scope="col"> <span> Goals</span></th></tr>
+                    <tr><th className='Title' colspan="4">  <h2> <span>Best</span> SCORERS </h2></th></tr>
+                    <th className='ColPlayer' scope="col"> <h3><span>Player</span></h3></th>
+                    <th className='Colgoals' scope="col"> <h3><span>Goals</span></h3></th>
                 </thead>
             </table>
-            {data}
+
         </div>
     )
 }
 
 export default TopScorerWidget
 
-
+// {data}
 /*
 import React from 'react';
 
