402aa6a40a802ab02e3e08711b34b15d5093f974
8rice <bricesabuco@gmail.com>
2021-11-14 20:14:53 +0100

    topscorer

--- frontend/src/Containers/TopScorerWidget.js
+++ frontend/src/Containers/TopScorerWidget.js
@@ -1,12 +1,12 @@
 // A RENOMMER PAR LE NOM DU WIDGET
 
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function TopScorerWidget() {
     const [data, setData] = useState(null);
 
-    function componentDidMount() {
+    useEffect(() => {
         fetch("https://v3.football.api-sports.io/players/topscorers?league=61&season=2020", {
             "method": "GET",
             "headers": {
@@ -16,16 +16,21 @@ function TopScorerWidget() {
             return response.json()
         })
             .then((result) => {
-                console.log(result.response[0].player.name)
-                setData(result.response[0].player.name)
+                let tab = [];
+                for (let x = 0; x < 5; x++) {
+                    console.log(result.response[x].player.name)
+                    tab.push(result.response[x].player.name)
+                }
+                setData(tab)
+
             })
-    }
-    componentDidMount();
+    });
+
     return (
         <div className='TopScorer'>
             <table>
                 <tr><th className='Title'
-                    colspan="2 "> <span>Best</span> SCORER</th></tr>
+                    colSpan="2 "> <span>Best</span> SCORER</th></tr>
                 <tr><th className='ColPlayer' scope="col"> <span>Player</span></th></tr>
                 <tr><th className='Colgoals' scope="col"> <span> Goals</span></th></tr>
 
