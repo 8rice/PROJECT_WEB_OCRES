f1ea8433a60ef85c470478d51d9a190f668dfc26
8rice <bricesabuco@gmail.com>
2021-11-23 17:02:41 +0100

    Top Scorer Widget Ligue 1

--- frontend/src/Containers/TopScorerWidget.js
+++ frontend/src/Containers/TopScorerWidget.js
@@ -1,13 +1,59 @@
 // A RENOMMER PAR LE NOM DU WIDGET
 
+//autre key : "c742f7706995d0d8efeb6d3002dc638b"
 
 import React, { useState, useEffect } from 'react'
 
+
 function TopScorerWidget() {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch("https://v3.football.api-sports.io/players/topscorers?league=61&season=2020", {
+        let mounted = true;
+
+        fetch("https://v3.football.api-sports.io/players/topscorers?league=61&season=2021", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "v3.football.api-sports.io",
+                "x-rapidapi-key": "f362f7602bfd94a69f2ccee18732fbd2"
+            }
+        })
+            .then(response => response.json())
+            .then((json) => {
+                if (mounted) {
+                    console.log(json.response)
+                    setData(json.response)
+                }
+            })
+        return () => mounted = false
+    }, []);
+
+    return (
+        <div className='TopScorer'>
+            <div>
+                <h2><span>Best</span> Scorers</h2>
+                <div className="Infos">
+                    <p>Player</p>
+                    <p>Goals</p>
+                </div>
+            </div>
+            <div className="Players">
+                {data.slice(0, 10).map(
+                    player =>
+                        <div className="PlayerStats">
+                            <p>{player.player.name}</p>
+                            <p>{player.statistics[0].goals.total}</p>
+                        </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default TopScorerWidget
+
+
+/*fetch("https://v3.football.api-sports.io/players/topscorers?league=61&season=2020", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "c742f7706995d0d8efeb6d3002dc638b",
@@ -22,72 +68,4 @@ function TopScorerWidget() {
                     tab.push(result.response[x].player.name)
                 }
                 setData(tab)
-            })
-    });
-
-    return (
-        <div className='TopScorer'>
-            <table>
-                <thead>
-                    <tr><th className='Title' colspan="3">  <h2> <span>Best</span> SCORERS </h2></th></tr>
-                    <tr><th className='ColPlayer'> <h3><span>Player</span></h3></th>
-                    <th className='Colgoals'><h3><span>Goals</span></h3></th> </tr>
-                </thead>
-
-                <tbody>
-                    <tr> <td colspan="3"><h4>Lionel Messi</h4> </td>  <td colspan="3"><h4>12</h4> </td> </tr>
-                    <tr> <td colspan="3"><h4>Robert Lewandowski</h4> </td>  <td colspan="3"><h4>12</h4> </td> </tr>
-                    <tr> <td colspan="3"><h4>Neymar </h4> </td>  <td colspan="3"><h4>7</h4> </td> </tr>
-                    <tr> <td colspan="3"><h4>Cristiano Ronaldo</h4> </td>  <td colspan="3"><h4>12</h4> </td> </tr>
-                    <tr> <td colspan="3"><h4>Kylian Mbappe</h4> </td>  <td colspan="3"><h4>12</h4> </td> </tr>
-                    <tr> <td colspan="3"><h4>Erling Haaland</h4> </td>  <td colspan="3"><h4>12</h4> </td> </tr>
-                    <tr> <td colspan="3"><h4>Jorginho</h4> </td>  <td colspan="3"><h4>12</h4> </td> </tr>
-                    <tr> <td colspan="3"><h4>Bruno Fernandes</h4> </td>  <td colspan="3"><h4>12</h4> </td> </tr>
-                </tbody>
-
-            </table>
-            {data}
-        </div>
-    )
-}
-
-export default TopScorerWidget
-
-// {data}
-/*
-import React from 'react';
-
-export default class Widget1 extends React.Component {
-    state = {
-        data: []
-    }
-
-    componentDidMount() {
-        fetch("https://v3.football.api-sports.io/players/topscorers", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "f362f7602bfd94a69f2ccee18732fbd2",
-            }
-        }).then((response) => {
-            return response.json()
-        })
-            .then((result) => {
-                this.setState(
-                    { data: result.response[0].player.name }
-                )
-                console.log(result.response[0].player.name)
-                console.log(result.response[0].statistics.goals.total)
-            })
-    }
-
-
-    render() {
-        return (
-            <div>
-                Hello
-                <p> {this.state.data}</p>
-            </div >
-        );
-    }
-}
-*/
+            })*/
