2e35bdf4312cd58bf64608c55c37d4214542fa10
Yannis Goore <yannis.goore@edu.ece.fr>
2021-12-05 16:35:33 +0100

    changement de port

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,10 @@ var ballondorRouter = require("./routes/ballondor");
 
 var app = express();
 
+var server = app.listen(8080, function() {
+    console.log('Ready on port %d', server.address().port);
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
